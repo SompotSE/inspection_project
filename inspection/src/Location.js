9591ea13b8050a3f1b792469d79d049dac57561e
SompotSE <s5802041620203@email.kmutnb.ac.th>
2018-10-15 20:19:30 +0700

    create Location

---
+++ inspection/src/Location.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Location extends Component {
+
+
+    render(){
+        return(
+            <div>
+                <pe>Location </pe>
+            </div>
+        );
+    }
+}
+
+export default Location;
