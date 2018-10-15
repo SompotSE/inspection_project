d5f66a45de5a6f6388f54f049420e6d87de08b5d
SompotSE <s5802041620203@email.kmutnb.ac.th>
2018-10-15 20:11:33 +0700

    create Home

---
+++ inspection/src/Home.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+
+
+    render(){
+        return(
+            <div>
+                <pe>Home </pe>
+            </div>
+        );
+    }
+}
+
+export default Home;
