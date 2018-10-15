8154b6f631e964b253d77a2c05c7d984b28bc9de
SompotSE <s5802041620203@email.kmutnb.ac.th>
2018-10-15 19:56:38 +0700

    create login

---
+++ inspection/src/login.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class login extends Component {
+
+
+    render(){
+        return(
+            <div>
+                <pe>Login </pe>
+            </div>
+        );
+    }
+}
+
+export default login;
