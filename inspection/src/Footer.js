0e49a76e236342e332806844df85a01247acc9bb
SompotSE <s5802041620203@email.kmutnb.ac.th>
2018-10-15 19:47:08 +0700

    Add projetc

---
+++ inspection/src/Footer.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+
+class Footer extends Component {
+
+
+    render(){
+        return(
+            <div>
+                <pe>ติดต่อฉัน : กลุ่มนักศึกษาชั้นปีที่ 4 สาขาเทคโนโลยีคอมพิวเตอร์</pe><br/>
+                <pe>ภาควิชาคอมพิวเตอร์ศึกษา คณะครุศาสตร์อุตสหกรรม มจพ.</pe>
+            </div>
+        );
+    }
+}
+
+export default Footer;
