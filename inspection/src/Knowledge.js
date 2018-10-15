89ad297499c41a2547be2100fa42412bc5d011b3
SompotSE <s5802041620203@email.kmutnb.ac.th>
2018-10-15 20:16:22 +0700

    create Knowledge

---
+++ inspection/src/Knowledge.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Knowledge extends Component {
+
+
+    render(){
+        return(
+            <div>
+                <pe>Knowledge </pe>
+            </div>
+        );
+    }
+}
+
+export default Knowledge;
