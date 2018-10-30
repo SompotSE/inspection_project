b8727b8bbd226f7bb29035d888b4317682bc19f5
SompotSE <s5802041620203@email.kmutnb.ac.th>
2018-10-30 15:50:44 +0700

    call Header use index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import Header from './Header';
 
 ReactDOM.render(
     <Header />,document.getElementById('root')
 );
-
-
-//serviceWorker.unregister();
