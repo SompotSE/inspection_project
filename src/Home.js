92cdc7e31959e9665418e64b5a28d521aad1e8f5
SompotSE <s5802041620203@email.kmutnb.ac.th>
2018-10-30 15:46:32 +0700

    Add test content

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,10 @@
 import React, {Component} from 'react';
-import Header from './Header';
-import Footer from './Footer';
-
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <Header/>
-        <p>sfsdfshdjfhsdkfhkjsdhfkjashdkjfhakjdsfhkjasdhfkjahsdkfjhaskdjfhkjsadhfkjsahd</p>
-        <Footer/>
+        <pe>sfsdfshdjfhsdkfhkjsdhfkjashdkjfhakjdsfhkjasdhfkjahsdkfjhaskdjfhkjsadhfkjsahd</pe>
       </div>
     );
   }
