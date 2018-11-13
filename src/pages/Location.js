74bbbdb55b314ee7de1a28bcc2dcafe6348c3bc0
SompotSE <s5802041620203@email.kmutnb.ac.th>
2018-11-14 01:03:57 +0700

    Add map

--- src/pages/Location.js
+++ src/pages/Location.js
@@ -1,15 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
 class Location extends Component {
 
 
-    render(){
-        return(
+    render() {
+        return (
             <div>
                 <pe>Location </pe>
+                <center>
+                    <Map google={this.props.google}
+                        style={{ width: '80%', height: '80%', position: 'relative' }}
+                        zoom={14}>
+                    </Map>
+                </center>
             </div>
         );
     }
 }
 
-export default Location;
+export default GoogleApiWrapper({
+    apiKey: ("AIzaSyDwUy4FtOC4ecPk8M7stYvqur-YPfrGFhA")
+}) (Location)
