97dfb9308434c9cbf3e335ccc91b881361ca833b
SompotSE <s5802041620203@email.kmutnb.ac.th>
2018-11-19 21:35:23 +0700

    Test map

--- src/pages/Location.js
+++ src/pages/Location.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
+var lntSet 
+var latSet 
+
+function SetMar () {
+   return <h1>Test</h1> 
+}
+    
 class Location extends Component {
 
-
+    
     render() {
         return (
             <div>
@@ -12,6 +19,8 @@ class Location extends Component {
                     <Map google={this.props.google}
                         style={{ width: '80%', height: '80%', position: 'relative' }}
                         zoom={14}>
+                        {/* <SetMar latSet="13.8185021" lntSet = "100.5141232" /> */}
+                        <SetMar />
                     </Map>
                 </center>
             </div>
@@ -20,5 +29,5 @@ class Location extends Component {
 }
 
 export default GoogleApiWrapper({
-    apiKey: ("AIzaSyDwUy4FtOC4ecPk8M7stYvqur-YPfrGFhA")
+    apiKey: ("AIzaSyAFHTcbUykLDkXfK19GoXmm8EltWUbq9dM")
 }) (Location)
