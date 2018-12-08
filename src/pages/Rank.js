e5cee79da3b5f1a0ed89bc09fa98e3e993d3ac7f
chanchai <chanchaiaun@hotmail.com>
2018-12-08 17:01:21 +0700

    addtoggleintable

--- src/pages/Rank.js
+++ src/pages/Rank.js
@@ -1,27 +1,29 @@
 import React, { Component } from 'react';
-var state=0;
+var state = 0;
 class Rank extends Component {
     constructor() {
         super();
         this.state = {
             megsage: "like",
             count: 0
+         
         };
         this.changVote = this.changVote.bind(this);
     }
     changVote() {
-        if (state==0){
-        this.setState({ count: this.state.count + 1 });
-        this.setState({megsage : "Unlike"});
-        state=1;
+        if (state == 0) {
+            this.setState({ count: this.state.count + 1 });
+            this.setState({ megsage: "Unlike" });
+            state = 1;
         }
-        else if (state==1){
+        else if (state == 1) {
             this.setState({ count: this.state.count - 1 });
-            this.setState({megsage : "like"});
-            state=0;
-            }
+            this.setState({ megsage: "like" });
+            state = 0;
+        }
     }
     
+
     render() {
         return (
             <div>
@@ -33,12 +35,15 @@ class Rank extends Component {
                     <table border="1" style={{ backgroundColor: 'white' }} bordercolor="gray">
                         <tr ><td width="300" className="titletable">ชื่อ ตรอ</td>
                             <td width="200" className="titletable">คะแนน</td>
+                            <td width="200" className="titletable">ให้คะแนน</td>
                         </tr>
-                        <tr><td width="300" >ตรอ ลอร่าวัน</td>
-                            <td width="200" ><h5>{this.state.count}</h5></td>
+                        <tr><td width="300" ><center>ตรอ ลอร่าวัน</center></td>
+                            <td width="200" ><center><h5>{this.state.count}</h5></center></td>
+                            <td><center><button onClick={this.changVote}>{this.state.megsage}</button></center></td>
                         </tr>
+                        
                     </table>
-                    <button onClick={this.changVote}>{this.state.megsage }</button>
+
                 </center>
             </div>
         );
