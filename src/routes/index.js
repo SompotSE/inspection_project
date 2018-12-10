b0ed035241995cb7fc5bc71ab04cecfb3f31b788
wan <s5802041620165@email.kmutnb.ac.th>
2018-12-10 18:00:28 +0700

    .

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,11 +11,6 @@ import Register from "../pages/Register";
 import ModalExample2 from "../pages/TestPopup";
 import ModalExample from "../pages/ModalExample";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Knowledge1 from "../pages/Knowledge1";
-import Knowledge2 from "../pages/Knowledge2";
-import Knowledge3 from "../pages/Knowledge3";
-import Knowledge4 from "../pages/Knowledge4";
-import Knowledge5 from "../pages/Knowledge5";
 
 export default () => (
     <Switch>
@@ -28,11 +23,7 @@ export default () => (
       <Route exact path="/test" component={ModalExample} />
       <Route exact path="/Register" component={Register} />
       <Route exact path="/TestPopup" component={ModalExample2} />
-      <Route exact path="/Knowledge/1" component={Knowledge1} />
-      <Route exact path="/Knowledge/2" component={Knowledge2} />
-      <Route exact path="/Knowledge/3" component={Knowledge3} />
-      <Route exact path="/Knowledge/4" component={Knowledge4} />
-      <Route exact path="/Knowledge/5" component={Knowledge5} />
+
 
     </Switch>
   )
