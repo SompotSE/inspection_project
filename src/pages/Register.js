4d438daa74bf100e5ad534951d33e60145a1b3ff
SiFlAMeSE <flame_uz@hotmail.com>
2018-12-11 05:01:32 +0700

    fix register

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -66,7 +66,7 @@ export default class Register extends React.Component {
                                             <Button>Submit</Button>
                                         </center>
                                     </Form>
-                                    <br />
+                                    <br /><br /><br /><br /><br />
                                 </div>
                             </div>
                         </Col>
